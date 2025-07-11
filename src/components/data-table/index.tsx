68713000689ffda3e9768925c83e895749cab284
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2025-07-11 14:49:21 +0100

    ♻️ (Remove Unuse Table Pagination)

--- src/components/data-table/index.tsx
+++ src/components/data-table/index.tsx
@@ -4,7 +4,7 @@ import {
   ColumnDef,
   getCoreRowModel,
   getFilteredRowModel,
-  getPaginationRowModel,
+  //getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
   flexRender,
@@ -111,9 +111,9 @@ export function DataTable<TData>({
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
-    getPaginationRowModel: isShowPagination
-    ? getPaginationRowModel()
-    : undefined,
+    // getPaginationRowModel: isShowPagination
+    // ? getPaginationRowModel()
+    // : undefined,
   });
 
   const selectedRows = table.getFilteredSelectedRowModel().rows;
